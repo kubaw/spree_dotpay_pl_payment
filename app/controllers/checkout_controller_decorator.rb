097318d82f0690f32afbc11539064b3e1b66408e
Jakub Woźny <jakub@wozny.net.pl>
2013-05-02 00:06:49 +0200

    Spree namespace

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -7,7 +7,7 @@ Spree::CheckoutController.class_eval do
   def redirect_for_dotpay_pl
     return unless params[:state] == "payment"
     @payment_method = Spree::PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id])
-    if @payment_method && @payment_method.kind_of?(PaymentMethod::DotpayPl)
+    if @payment_method && @payment_method.kind_of?(Spree::PaymentMethod::DotpayPl)
       redirect_to gateway_dotpay_pl_path(:gateway_id => @payment_method.id, :order_id => @order.id)
     end
   end
